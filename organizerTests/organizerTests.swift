b969670ec747e56d1f8165a13a83ac7731e91e24
Hey-Scotty <Scotty@Tevins-MacBook-Pro.local>
2017-01-08 21:05:15 -0500

    Initial Commit

---
+++ organizerTests/organizerTests.swift
@@ -0,0 +1,36 @@
+//
+//  organizerTests.swift
+//  organizerTests
+//
+//  Created by Tevin Scott on 1/8/17.
+//  Copyright © 2017 Tevin Scott. All rights reserved.
+//
+
+import XCTest
+@testable import organizer
+
+class organizerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
